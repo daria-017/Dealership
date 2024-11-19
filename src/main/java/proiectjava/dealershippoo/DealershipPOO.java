a6fd0fa8a7504f2411e937ba193ef8b10515b167
Daria Rata <rata.daria1999@gmail.com>
2024-11-19 18:05:55 +0200

    First Commit!

---
+++ src/main/java/proiectjava/dealershippoo/DealershipPOO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package proiectjava.dealershippoo;
+
+/**
+ *
+ * @author Daria
+ */
+public class DealershipPOO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
