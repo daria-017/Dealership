c9f9e78178cbe9550dc41f3a03eca003f1b229f0
Daria Rata <rata.daria1999@gmail.com>
2024-11-19 18:53:14 +0200

    First Commit!

---
+++ src/main/java/proiectjava/dealershippoo/Airplane.java
@@ -0,0 +1,8 @@
+//JetPlane, Airplane, Helicopter
+//Vehicle, Customer, VehicleShop
+package proiectjava.dealershippoo;
+//eu lucrez aici in local si dupa cum fac?
+//
+public class Airplane {
+    
+}
