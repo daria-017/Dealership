2d35dee0160d8b232e18bc48cde0b0b7ccb7d9cb
Daria Rata <rata.daria1999@gmail.com>
2024-11-20 18:45:24 +0200

    First Commit!

--- src/main/java/proiectjava/dealershippoo/AirVehicles.java
+++ src/main/java/proiectjava/dealershippoo/AirVehicles.java
@@ -4,10 +4,50 @@
  */
 package proiectjava.dealershippoo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Daria
  */
 public class AirVehicles extends Vehicle{
+    String type;
+    ArrayList<Vehicle> vehicles;
+    
+    AirVehicles(){
+        type="Neprecizat";
+        vehicles=null;
+    }
+    AirVehicles(String type, ArrayList<Vehicle> vehicles){
+        this.type=type;
+        this.vehicles=vehicles;
+    }
+    AirVehicles(AirVehicles airvehicles){
+        this.type=airvehicles.type;
+        this.vehicles=airvehicles.vehicles;
+    }
+    public String toString(){
+        return "Tipul de vehicul: "+type+"\nVehicule: "+vehicles;
+    }
+    
+    public String getType(){
+        return type;
+    }
+    
+    public ArrayList<Vehicle> getVehicles(){
+        return vehicles;
+    }
+    
+    
+    public void setType(String type){
+        this.type=type;
+    }
+    
+    public void setVehicles(ArrayList<Vehicle> vehicles){
+        this.vehicles=vehicles;
+    }
+    
+    
+    
     
 }
