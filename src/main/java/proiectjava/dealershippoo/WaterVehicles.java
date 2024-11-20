e78de29b4ca8c367f36e609f3f831f78f5761087
Dragos <dragosxp715@gmail.com>
2024-11-20 15:26:39 +0200

    v 1.0.0 - Clasa WaterVehicles si metodele sale - Dragos :>

--- src/main/java/proiectjava/dealershippoo/WaterVehicles.java
+++ src/main/java/proiectjava/dealershippoo/WaterVehicles.java
@@ -1,10 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package proiectjava.dealershippoo;
 
+import java.util.ArrayList;
 
+/**
+ *
+ * @author Dragos :>
+ */
 public class WaterVehicles extends Vehicle {
-    //modificare
+
+    String type;
+    ArrayList<Vehicle> vehicles;
+
+    WaterVehicles() {
+        type = "<gol>";
+        vehicles = null;
+    }
+
+    WaterVehicles(String type, ArrayList<Vehicle> vehicles) {
+        this.type = type;
+        this.vehicles = vehicles;
+    }
+
+    WaterVehicles(WaterVehicles wv) {
+        this.type = wv.type;
+        this.vehicles = wv.vehicles;
+    }
+
+    @Override
+    public String toString() {
+        return "Tip: " + type + "\nVehicule: " + vehicles;
+    }
+    
+    public String getType()
+    {
+        return type;
+    }
+    
+    public void setType(String type)
+    {
+        this.type = type;
+    }
+    
+    public ArrayList<Vehicle> getVehicles()
+    {
+        return vehicles;
+    }
+    
+    public void setVehicles(ArrayList<Vehicle> vehicles)
+    {
+        this.vehicles = vehicles;
+    }
 }
